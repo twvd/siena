f548b41167d9ab0cbf42a83d946f75a6f336863a
Thomas <thomas@thomasw.dev>
2023-10-09 22:31:34 +0200

    Add CPU skeleton, instruction fetching

--- src/bin/souper/main.rs
+++ src/bin/souper/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::io::{stdin, Read};
+
+use anyhow::{bail, Result};
+
+use souper::snes::bus::testbus::Testbus;
+use souper::snes::bus::BusMember;
+use souper::snes::cpu_65816::cpu::Cpu65816;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        bail!("Syntax: {} <filename>", args[0]);
+    }
+
+    let f = fs::read(&args[1])?;
+
+    let mut bus = Testbus::new();
+
+    bus.write_slice(&f[0x200..], 0x8000);
+    let reset = bus.read16(0xFFFC);
+
+    let mut cpu = Cpu65816::<Testbus>::new(bus, reset);
+
+    loop {
+        println!("{}", cpu.dump_state());
+
+        let _ = stdin().read(&mut [0u8]).unwrap();
+
+        cpu.step()?;
+    }
 }
---
+++ src/snes/cpu_65816/cpu.rs
@@ -0,0 +1,92 @@
+use anyhow::Result;
+
+use crate::snes::bus::{Address, Bus, BusIterator};
+use crate::tickable::Ticks;
+
+use super::instruction::Instruction;
+use super::regs::{Flag, RegisterFile};
+
+/// Main SNES CPU (65816)
+pub struct Cpu65816<TBus: Bus> {
+    bus: TBus,
+    regs: RegisterFile,
+    cycles: Ticks,
+}
+
+impl<TBus> Cpu65816<TBus>
+where
+    TBus: Bus,
+{
+    pub fn new(bus: TBus, reset_addr: u16) -> Self {
+        let mut cpu = Self {
+            bus,
+            regs: RegisterFile::new(),
+            cycles: 0,
+        };
+        cpu.regs.pc = reset_addr;
+        cpu
+    }
+
+    pub fn dump_state(&self) -> String {
+        format!(
+            "{} - {}\n --> {}",
+            self.cycles,
+            self.regs,
+            self.peek_next_instr().unwrap()
+        )
+    }
+
+    /// Fetches and decodes the next instruction at PC
+    pub fn peek_next_instr(&self) -> Result<Instruction> {
+        let mut busiter = BusIterator::new_from(&self.bus, self.regs.get_full_pc());
+        Instruction::decode(
+            &mut busiter,
+            self.regs.test_flag(Flag::M),
+            self.regs.test_flag(Flag::X),
+        )
+    }
+
+    /// Fetches and decodes the next instruction at PC
+    pub fn fetch_next_instr(&mut self) -> Result<Instruction> {
+        let mut fetched: Vec<u8> = vec![];
+
+        for p in 0.. {
+            match Instruction::decode(
+                &mut fetched.clone().into_iter(),
+                self.regs.test_flag(Flag::M),
+                self.regs.test_flag(Flag::X),
+            ) {
+                Err(_) => fetched.push(self.read_tick(self.regs.get_full_pc().wrapping_add(p))),
+                Ok(i) => return Ok(i),
+            }
+        }
+
+        unreachable!()
+    }
+
+    /// Executes one CPU step (one instruction).
+    pub fn step(&mut self) -> Result<Ticks> {
+        let instr = self.fetch_next_instr()?;
+        // TODO program bank?
+        self.regs.pc = self.regs.pc.wrapping_add(instr.len as u16);
+
+        Ok(1)
+    }
+
+    /// Tick peripherals
+    fn tick_bus(&mut self, cycles: Ticks) -> Result<()> {
+        if cycles == 0 {
+            return Ok(());
+        }
+
+        self.bus.tick(cycles)
+    }
+
+    /// Reads a memory location while ticking peripherals
+    /// for the access time.
+    fn read_tick(&mut self, addr: Address) -> u8 {
+        let v = self.bus.read(addr);
+        self.tick_bus(1).unwrap();
+        v
+    }
+}
