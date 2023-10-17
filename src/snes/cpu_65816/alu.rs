bf75021eef7f2cd62327a96040c0802514b86bd8
Thomas <thomas@thomasw.dev>
2023-10-17 12:59:43 +0200

    65618: implement SBC (except immediate mode)

--- src/snes/cpu_65816/alu.rs
+++ src/snes/cpu_65816/alu.rs
@@ -1,3 +1,4 @@
+/// Result of an ALU operation
 pub struct AluResult {
     pub result: u16,
     pub c: bool,
@@ -6,6 +7,7 @@ pub struct AluResult {
     pub v: bool,
 }
 
+/// 16-bit add (decimal mode)
 pub fn add16_dec(acc: u16, d: u16, carry: bool) -> AluResult {
     let acc: u32 = acc.into();
     let d: u32 = d.into();
@@ -42,6 +44,7 @@ pub fn add16_dec(acc: u16, d: u16, carry: bool) -> AluResult {
     }
 }
 
+/// 16-bit add (binary mode)
 pub fn add16(acc: u16, d: u16, carry: bool) -> AluResult {
     let c = if carry { 1 } else { 0 };
 
@@ -56,6 +59,7 @@ pub fn add16(acc: u16, d: u16, carry: bool) -> AluResult {
     }
 }
 
+/// 8-bit add (binary mode)
 pub fn add8(acc: u16, d: u16, carry: bool) -> AluResult {
     let c = if carry { 1 } else { 0 };
 
@@ -70,6 +74,7 @@ pub fn add8(acc: u16, d: u16, carry: bool) -> AluResult {
     }
 }
 
+/// 8-bit add (decimal mode)
 pub fn add8_dec(acc: u16, d: u16, carry: bool) -> AluResult {
     let mut c = if carry { 1 } else { 0 };
     let mut result: u16 = (acc & 0x000F) + (d & 0x000F) + c;
@@ -89,5 +94,98 @@ pub fn add8_dec(acc: u16, d: u16, carry: bool) -> AluResult {
         n: result & 0x80 != 0,
         v,
         c: result > u8::MAX.into(),
+    }
+}
+
+/// 16-bit subtract (binary mode)
+pub fn sub16(acc: u16, d: u16, carry: bool) -> AluResult {
+    let c = if carry { 1 } else { 0 };
+
+    let result: i32 = acc as i32 + (!d) as i32 + c;
+
+    AluResult {
+        result: result as u16,
+        z: result as u16 == 0,
+        n: result & 0x8000 != 0,
+        v: !(acc ^ !d) & (acc ^ result as u16) & 0x8000 != 0,
+        c: result > u16::MAX.into(),
+    }
+}
+
+/// 8-bit subtract (binary mode)
+pub fn sub8(acc: u16, d: u16, carry: bool) -> AluResult {
+    let c = if carry { 1 } else { 0 };
+
+    let result = acc.wrapping_add(!d & 0xFF).wrapping_add(c);
+
+    AluResult {
+        result: result & 0xFF,
+        z: result as u8 == 0,
+        n: result & 0x80 != 0,
+        v: !(acc ^ !d) & (acc ^ result) & 0x80 != 0,
+        c: result > u8::MAX.into(),
+    }
+}
+
+/// 8-bit subtract (decimal mode)
+pub fn sub8_dec(acc: u16, d: u16, carry: bool) -> AluResult {
+    let d = !d as i16;
+    let mut c = if carry { 1 } else { 0 };
+    let acc = acc as i16;
+    let mut result: i16 = (acc & 0x000F) + (d & 0x000F) + c;
+    if result <= 0x000F {
+        result = result - 0x06;
+    }
+    c = if result > 0x000F { 1 } else { 0 };
+    result = (acc & 0x00F0) + (d & 0x00F0) + (c << 4) + (result & 0x000F);
+    let v = !(acc ^ d) & (acc ^ result) & 0x80 != 0;
+    if result <= 0x00FF {
+        result = result - 0x60;
+    }
+
+    AluResult {
+        result: result as u16 & 0xFF,
+        z: result as u8 == 0,
+        n: result & 0x80 != 0,
+        v,
+        c: result > u8::MAX.into(),
+    }
+}
+
+/// 16-bit subtract (decimal mode)
+pub fn sub16_dec(acc: u16, d: u16, carry: bool) -> AluResult {
+    let acc = acc as i32;
+    let d = !d as i32;
+    let acc = acc as i32;
+    let mut c = if carry { 1 } else { 0 };
+    let mut result: i32 = (acc & 0x000F) + (d & 0x000F) + c;
+    if result <= 0x000F {
+        result = result - 0x0006;
+    }
+    c = if result > 0x000F { 1 } else { 0 };
+    result = (acc & 0x00F0) + (d & 0x00F0) + (c << 4) + (result & 0x000F);
+    if result <= 0x00FF {
+        result = result - 0x0060;
+    }
+
+    c = if result > 0x00FF { 1 } else { 0 };
+    result = (acc & 0x0F00) + (d & 0x0F00) + (c << 8) + (result & 0x00FF);
+    if result <= 0x0FFF {
+        result = result - 0x0600;
+    }
+    c = if result > 0x0FFF { 1 } else { 0 };
+
+    result = (acc & 0xF000) + (d & 0xF000) + (c << 12) + (result & 0x0FFF);
+    let v = !(acc ^ d) & (acc ^ result) & 0x8000 != 0;
+    if result <= 0xFFFF {
+        result = result - 0x6000;
+    }
+
+    AluResult {
+        result: result as u16,
+        z: result as u16 == 0,
+        n: result & 0x8000 != 0,
+        v,
+        c: result > u16::MAX.into(),
     }
 }
--- src/snes/cpu_65816/cpu.rs
+++ src/snes/cpu_65816/cpu.rs
@@ -208,6 +208,7 @@ where
             InstructionType::REP => self.op_rep(&instr),
             InstructionType::SEP => self.op_sep(&instr),
             InstructionType::ADC => self.op_add(&instr),
+            InstructionType::SBC => self.op_sbc(&instr),
 
             _ => todo!(),
         }
@@ -633,10 +634,15 @@ where
             _ => (),
         }
 
-        let data = if self.regs.test_flag(Flag::M) {
+        let data: u16 = if self.regs.test_flag(Flag::M) {
             self.read_tick(addr).into()
         } else {
-            self.read16_tick_a16(addr).into()
+            match instr.def.mode {
+                AddressingMode::Direct | AddressingMode::DirectX | AddressingMode::StackS => {
+                    self.read16_tick_a16(addr)
+                }
+                _ => self.read16_tick_a24(addr),
+            }
         };
 
         let result = match (self.regs.test_flag(Flag::D), self.regs.test_flag(Flag::M)) {
@@ -656,6 +662,106 @@ where
                 self.regs.test_flag(Flag::C),
             ),
             (true, true) => alu::add8_dec(
+                self.regs.read(Register::A),
+                data,
+                self.regs.test_flag(Flag::C),
+            ),
+        };
+
+        if self.regs.test_flag(Flag::M) {
+            // 8-bit
+            self.regs.write(Register::A, result.result);
+        } else {
+            // 16-bit
+            self.regs.write(Register::C, result.result);
+        }
+        self.regs.write_flags(&[
+            (Flag::N, result.n),
+            (Flag::V, result.v),
+            (Flag::Z, result.z),
+            (Flag::C, result.c),
+        ]);
+
+        Ok(())
+    }
+
+    /// SBC - SuBtract (accumulator)
+    fn op_sbc(&mut self, instr: &Instruction) -> Result<()> {
+        // Internal cycles for adding D, if applicable (D > 0).
+        if self.regs.read(Register::DL) != 0 {
+            match instr.def.mode {
+                AddressingMode::Direct
+                | AddressingMode::DirectPtr16
+                | AddressingMode::DirectPtr16Y
+                | AddressingMode::DirectPtr24
+                | AddressingMode::DirectPtr24Y
+                | AddressingMode::DirectXPtr16
+                | AddressingMode::DirectX
+                | AddressingMode::DirectY => self.tick_bus(1)?,
+                _ => (),
+            };
+        }
+
+        // Extra internal cycles for modes that add a register
+        // to the address before resolution.
+        match instr.def.mode {
+            AddressingMode::DirectX
+            | AddressingMode::DirectXPtr16
+            | AddressingMode::DirectY
+            | AddressingMode::StackS
+            | AddressingMode::StackSPtr16Y => self.tick_bus(1)?,
+            _ => (),
+        }
+
+        let (addr, crosses_page) = self.resolve_address(instr)?;
+
+        // Extra cycle if crossing a page boundary || !X
+        match instr.def.mode {
+            AddressingMode::AbsoluteX
+            | AddressingMode::AbsoluteY
+            | AddressingMode::DirectPtr16Y => {
+                if !self.regs.test_flag(Flag::X) || crosses_page {
+                    self.tick_bus(1)?
+                }
+            }
+            _ => (),
+        }
+
+        // More internal cycles for modes that do stuff AFTER
+        // address resolution bus activity.
+        match instr.def.mode {
+            AddressingMode::StackSPtr16Y => self.tick_bus(1)?,
+            _ => (),
+        }
+
+        let data: u16 = if self.regs.test_flag(Flag::M) {
+            self.read_tick(addr).into()
+        } else {
+            match instr.def.mode {
+                AddressingMode::Direct | AddressingMode::DirectX | AddressingMode::StackS => {
+                    self.read16_tick_a16(addr)
+                }
+                _ => self.read16_tick_a24(addr),
+            }
+        };
+
+        let result = match (self.regs.test_flag(Flag::D), self.regs.test_flag(Flag::M)) {
+            (true, false) => alu::sub16_dec(
+                self.regs.read(Register::C),
+                data,
+                self.regs.test_flag(Flag::C),
+            ),
+            (false, false) => alu::sub16(
+                self.regs.read(Register::C),
+                data,
+                self.regs.test_flag(Flag::C),
+            ),
+            (false, true) => alu::sub8(
+                self.regs.read(Register::A),
+                data,
+                self.regs.test_flag(Flag::C),
+            ),
+            (true, true) => alu::sub8_dec(
                 self.regs.read(Register::A),
                 data,
                 self.regs.test_flag(Flag::C),
