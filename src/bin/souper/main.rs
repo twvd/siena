863d16c76d0475c42dfa4054dc5d44195b8ca1ff
Thomas <thomas@thomasw.dev>
2023-10-22 21:12:06 +0200

    Add argument parsing

--- src/bin/souper/main.rs
+++ src/bin/souper/main.rs
@@ -3,19 +3,34 @@ use std::fs;
 use std::io::{stdin, Read};
 
 use anyhow::{bail, Result};
+use clap::Parser;
 
 use souper::snes::bus::mainbus::Mainbus;
 use souper::snes::bus::BusMember;
 use souper::snes::cpu_65816::cpu::Cpu65816;
 
+#[derive(Parser)]
+#[command(
+    about = "SNES Emulator",
+    author = "Thomas <thomas@thomasw.dev>",
+    long_about = None)]
+struct Args {
+    /// ROM filename to load.
+    filename: String,
+
+    /// Wait for keystroke after each CPU step.
+    #[arg(short, long)]
+    pause: bool,
+
+    /// Print CPU state after each instruction
+    #[arg(short, long)]
+    verbose: bool,
+}
+
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        bail!("Syntax: {} <filename>", args[0]);
-    }
-
-    let f = fs::read(&args[1])?;
+    let f = fs::read(args.filename)?;
 
     let bus = Mainbus::new(&f);
 
@@ -23,9 +38,13 @@ fn main() -> Result<()> {
     let mut cpu = Cpu65816::<Mainbus>::new(bus, reset);
 
     loop {
-        println!("{}", cpu.dump_state());
+        if args.verbose {
+            println!("{}", cpu.dump_state());
+        }
 
-        let _ = stdin().read(&mut [0u8]).unwrap();
+        if args.pause {
+            let _ = stdin().read(&mut [0u8]).unwrap();
+        }
 
         cpu.step()?;
     }
